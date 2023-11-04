58a89cd6bf94693397d940e0171009144ffb3692
de-mawo <groovebassist1@gmail.com>
2023-11-04 18:44:50 +0200

    Experimenting

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
-
 from flask import Flask
 from config import Config
 from app.extensions import db
 from flask_migrate import Migrate
 
 migrate = Migrate()
+
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -16,21 +16,23 @@ def create_app(config_class=Config):
 
     # Register blueprints
     from app.main import bp as main_bp
+
     app.register_blueprint(main_bp)
 
     from app.user import bp as user_bp
-    app.register_blueprint(user_bp, url_prefix='/user')
+
+    app.register_blueprint(user_bp, url_prefix="/user")
 
     # with app.app_context():
     #     from app.models.user import User
     #     # Add other model imports here
 
     #     # Create the database tables
-        print('Creating tables')
+    print("Creating tables")
     #     db.create_all()
-        
-        # Confirm that tables have been created
-        print("Database tables have been created.") 
-        print(Config.SQLALCHEMY_DATABASE_URI)     
+
+    # Confirm that tables have been created
+    print("Database tables have been created.")
+    print(Config.SQLALCHEMY_DATABASE_URI)
 
     return app
--- app/models/user.py
+++ app/models/user.py
@@ -4,31 +4,6 @@ from datetime import datetime
 from app.extensions import db
 
 
-<<<<<<< HEAD
-class User():
-    __tablename__ = 'users'
-    id = Column(UUID(as_uuid=True), primary_key=True,
-                default=uuid.uuid4, unique=True)
-    name = Column(String, nullable=False)
-    email = Column(String, nullable=False)
-    phone = Column(String, nullable=False)
-    lastname = Column(String)
-    profile_img = Column(String)
-    password = Column(String, nullable=False)
-    role = Column(String, default='USER')
-
-    def __init__(self, name, email, phone, password, lastname=None, profile_img=None, role='USER'):
-        self.name = name
-        self.email = email
-        self.phone = phone
-        self.lastname = lastname
-        self.profile_img = profile_img
-        self.password = password
-        self.role = role
-
-    def __repr__(self):
-        return f"User('{self.name}', '{self.email}', '{self.phone}', '{self.role}')"
-=======
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.String(50), primary_key=True,
@@ -51,4 +26,3 @@ class User(db.Model):
 
     # def __repr__(self):
     #     return f"User('{self.name}', '{self.email}', '{self.phone}', '{self.role}')"
->>>>>>> prince
